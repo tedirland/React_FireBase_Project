2dfc017f465a744f9eade2fd82c91d72d26acc0f
Ted Irland <irlandth@gmail.com>
2021-05-25 09:34:23 -0500

    Refactored functional component into a class component and took some notes on the main differences

--- servicario/src/pages/Home.js
+++ servicario/src/pages/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h1>Home Page</h1>
-    </div>
-  );
-};
+//Functional Components //
+//1. Smaller components
+// 2. Usually Presentational
+// 3. In most cases, reusable components
+// 4. Should focus on one responsibility
+
+// const Home = () => {
+//   return (
+//     <div className="container">
+//       <h1>Home Page</h1>
+//     </div>
+//   );
+// };
+
+//Class Components//
+//1. Larger components
+//2. Container components (contain other components)
+//3. Easier to handle lots of state
+//4. More boilerplate
+//5. Access to lifecycle functions
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>I am HomePage class</h1>
+      </div>
+    );
+  }
+}
 
 export default Home;
