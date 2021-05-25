a043a48bebbc1854fe523abbfcd7e7cdca7b1bd4
Ted Irland <irlandth@gmail.com>
2021-05-25 10:39:29 -0500

    Added in useEffect hook to functional component to manage state

--- servicario/src/pages/Home.js
+++ servicario/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 
 //Functional Components //
@@ -7,13 +7,26 @@ import './Home.css';
 // 3. In most cases, reusable components
 // 4. Should focus on one responsibility
 
-// const Home = () => {
-//   return (
-//     <div className="container">
-//       <h1>Home Page</h1>
-//     </div>
-//   );
-// };
+const Home = () => {
+  const [message, setMessage] = useState('Super Message!!');
+  // const messageState = useState('Super Message!!');
+  // const message = messageState[0];
+  // const setMessage = messageState[1];
+
+  //Called after component is initilized AND after it is updated
+  //Side effect - elements of state that will cause the useEffect hook to be called when updated
+  useEffect(() => {
+    setTimeout(() => {
+      setMessage('I am updated Message!');
+    }, 1000);
+  }, [message]);
+  return (
+    <div className="container">
+      <h1>Home Page</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
 
 //Class Components//
 //1. Larger components
@@ -22,34 +35,34 @@ import './Home.css';
 //4. More boilerplate
 //5. Access to lifecycle functions
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: 'Super Message!!!',
-    };
-  }
-  // state = {
-  //   message: 'Super Message !!!',
-  // };
+// class Home extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       message: 'Super Message!!!',
+//     };
+//   }
+//   // state = {
+//   //   message: 'Super Message !!!',
+//   // };
 
-  // Lifecycle function
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ message: 'I am updated Message' });
-    }, 1000);
-  }
-  // Lifecycle function
-  //Render is called every time the state is updated
-  render() {
-    const { message } = this.state;
-    return (
-      <div className="container">
-        <h1>I am HomePage class</h1>
-        <p>{message}</p>
-      </div>
-    );
-  }
-}
+//   // Lifecycle function
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ message: 'I am updated Message' });
+//     }, 1000);
+//   }
+//   // Lifecycle function
+//   //Render is called every time the state is updated
+//   render() {
+//     const { message } = this.state;
+//     return (
+//       <div className="container">
+//         <h1>I am HomePage class</h1>
+//         <p>{message}</p>
+//       </div>
+//     );
+//   }
+// }
 
 export default Home;
